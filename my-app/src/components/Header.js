1785b52abfd9d75d581c04d599490157d460b422
Irina Saveleva <goyalion17@gmail.com>
2021-04-30 12:44:22 +0300

    Base structure

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://freesvg.org/img/Fox-vector-logo.png" />
+    </header>
+  );
+};
+
+export default Header
