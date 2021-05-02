dc19ca52ce6cf6588b7d07e0d50165a371598277
Irina Saveleva <goyalion17@gmail.com>
2021-05-02 17:10:32 +0300

    18. Dialogs page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,7 +4,7 @@ import s from "./Profile.module.css";
 
 const Profile = () => {
   return (
-    <div className={s.content}>
+    <div>
       <div>
         <img src="https://global-uploads.webflow.com/5ef5480befd392489dacf544/5f9f5e5943de7e69a1339242_5f44a7398c0cdf460857e744_img-image.jpeg" />
       </div>
