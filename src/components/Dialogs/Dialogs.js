ae9b1a842b607ee1815a3b57d7c0405ec39ae9d0
Irina Saveleva <goyalion17@gmail.com>
2021-05-08 16:01:28 +0300

    21. Dialogs page

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -3,8 +3,20 @@ import s from "./Dialogs.module.css"
 
 const Dialogs = (props) => {
   return (
-    <div >
-      Dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + ' ' + s.active}>Dimych</div>
+        <div className={s.dialog}>Irinka</div>
+        <div className={s.dialog}>Sveta</div>
+        <div className={s.dialog}>Sasha</div>
+        <div className={s.dialog}>Viktor</div>
+        <div className={s.dialog}>Valera</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>Hello</div>
+        <div className={s.message}>What's up</div>
+      </div>
     </div>
   );
   
