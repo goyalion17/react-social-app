d8ca40cc7e06de1fea3544a0173dc2899a1e6e72
Irina Saveleva <goyalion17@gmail.com>
2021-05-09 15:11:44 +0300

    27. component vs render

--- src/App.js
+++ src/App.js
@@ -8,14 +8,21 @@ import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 
 const App = () => {
+  let posts = [
+    { id: 1, message: "Hi, how are you?", likesCount: 15 },
+    { id: 2, message: "It's my first post", likesCount: 35 },
+    { id: 2, message: "It's my first post", likesCount: 35 },
+    { id: 2, message: "It's my first post", likesCount: 35 },
+  ];
+
   return (
     <BrowserRouter>
       <div className="app-wrapper">
         <Header />
         <Navbar />
         <div className="app-wrapper-content">
-          <Route path="/profile" component={Profile} />
-          <Route path="/dialogs" component={Dialogs} />
+          <Route path="/dialogs" render={() => <Dialogs />} />
+          <Route path="/profile" render={() => <Profile posts={posts}/>} />
         </div>
       </div>
     </BrowserRouter>
