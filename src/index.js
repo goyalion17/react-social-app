c97109f30139a4c623e5c83a9ca9bc40f2937c8e
Irina Saveleva <goyalion17@gmail.com>
2021-05-11 13:03:21 +0300

    35. callback, subscribe, observer

--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import state from "./redux/state"
-import {addPost} from "./redux/state"
-import {rerenderEntireTree} from "./render"
+import state, { subscribe } from "./redux/state";
+import { addPost, updateNewPostText } from "./redux/state";
 
+let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App
+        state={state}
+        addPost={addPost}
+        updateNewPostText={updateNewPostText}
+      />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+};
 
-rerenderEntireTree(state)
+rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import { rerenderEntireTree } from "../render";
+let rerenderEntireTree = () => {
+  console.log("State is changed");
+};
 
 let state = {
   profilePage: {
@@ -39,7 +41,7 @@ export let addPost = () => {
   };
 
   state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = ''
+  state.profilePage.newPostText = "";
   rerenderEntireTree(state);
 };
 
@@ -47,5 +49,9 @@ export let updateNewPostText = (newText) => {
   state.profilePage.newPostText = newText;
   rerenderEntireTree(state);
 };
+
+export const subscribe =(observer)=> {
+  rerenderEntireTree = observer
+}
 
 export default state;
